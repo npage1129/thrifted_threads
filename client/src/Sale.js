ef0edbe29a4c059db2c3ca8916e2d6abe6f751e9
npage1129 <82468860+npage1129@users.noreply.github.com>
2022-05-10 11:06:05 -0600

    before we break it

--- client/src/Sale.js
+++ client/src/Sale.js
@@ -7,6 +7,11 @@ function Sale() {
         <div>
             <Header />
             <h1>I am Sales</h1>
+            {/* div className="cards">
+                            {item.on_sale ? <img src={item.img}/> : null}
+                            {item.on_sale ?<div className="name" id="inCard">{item.name} <br></br><br></br>${item.price}</div>: null }
+                            {item.on_sale ?<button className="cart">Add To Cart</button> : null}
+                        </div> */}
             <Footer />
         </div>
     )
